ac6e2dbe7a6ef166a87ed0c54dcdb6a559c3e40f
hi <138982320+hello-world-html@users.noreply.github.com>
2023-10-12 18:03:03 +0800

    0

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+document.write('hello world')
+var num = 10;
+console.log(num);
+var hello = null;
+var world = undefined;
+console.log(hello);
+console.log(world);
+var op = true;
+switch (op == true) {
+    case 0:
+        document.write('true');
+        break;
+    case 1:
+        document.write('false');
+
+    default:
+        document.write('false');
+}
