24525e477f13d36e480f48366ced0b5ec3cf8135
hi <138982320+hello-world-html@users.noreply.github.com>
2023-11-18 15:41:14 +0800

    nu'lnul

--- test/script.js
+++
@@ -1,6 +0,0 @@
-let box = document.querySelector('.box')
-for (let i = 0; i < 120; i++) {
-    let lis = document.createElement('div')
-    lis.className = "jie"
-    box.appendChild(lis)
-}
---
+++ test/script/script.js
@@ -0,0 +1,6 @@
+function tiaozhuan1() {
+window.location.href = "./Unit/1/1.html";
+}
+function tiaozhuan2(){
+    window.location.href = "./Unit/2/2.html";
+}
